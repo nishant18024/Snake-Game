0c18954b8ebd8b58cb3933f43433ff3ef7a8a8cb
Nishant Kumar <67552233+nishant18024@users.noreply.github.com>
2024-01-04 22:04:08 +0530

    Fixed Snake Movement Error

--- app.js
+++ app.js
@@ -14,121 +14,59 @@ function draw() {
         context.fillStyle = 'red';
         context.fillRect(cell[0], cell[1], cellSize, cellSize);
     }
-
 }
 
-// function to update snake length
+// Function to update snake length and direction
 function update() {
     let headX = snakeCell[snakeCell.length - 1][0];
     let headY = snakeCell[snakeCell.length - 1][1];
 
-    let newHeadX = headX + cellSize;
-    let newHeadY = headY;
+    let newHeadX, newHeadY;
+
+    // this can be changed
+    switch (direction) {
+        case 'up':
+            newHeadX = headX;
+            newHeadY = headY - cellSize;
+            break;
+        case 'down':
+            newHeadX = headX;
+            newHeadY = headY + cellSize;
+            break;
+        case 'left':
+            newHeadX = headX - cellSize;
+            newHeadY = headY;
+            break;
+        case 'right':
+            newHeadX = headX + cellSize;
+            newHeadY = headY;
+            break;
+    }
 
     snakeCell.push([newHeadX, newHeadY]);
     snakeCell.shift();
-
 }
 
-// Cdode for movement of Snake
+// Code for handling arrow key events
 document.body.addEventListener('keydown', function (event) {
-    if (key == 'ArrowUp') {
-        
+    switch (event.key) {
+        case 'ArrowUp':
+            direction = 'up';
+            break;
+        case 'ArrowDown':
+            direction = 'down';
+            break;
+        case 'ArrowLeft':
+            direction = 'left';
+            break;
+        case 'ArrowRight':
+            direction = 'right';
+            break;
     }
-    else if (key == 'ArrowDown') {
+});
 
-    }
-    else if (key == 'ArrowRight') {
-
-    }
-    else {
-
-    }
-})
-
-// It will update the snake after an interval of 2 ms
+// It will update the snake after an interval of 200 ms
 setInterval(function () {
     update();
     draw();
-}, 200)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// context.strokeStyle = 'Blue';
-// context.fillStyle = 'blue';
-// context.beginPath();
-
-// // Row
-// context.moveTo(0, 100);
-// context.lineTo(1000, 100);
-// context.moveTo(0, 200);
-// context.lineTo(1000, 200);
-// context.moveTo(0, 300);
-// context.lineTo(1000, 300);
-// context.moveTo(0, 400);
-// context.lineTo(1000, 400);
-// context.moveTo(0, 500);
-// context.lineTo(1000, 500);
-
-// // Column
-// context.moveTo(100, 0);
-// context.lineTo(100, 600);
-// context.moveTo(200, 0);
-// context.lineTo(200, 600);
-// context.moveTo(300, 0);
-// context.lineTo(300, 600);
-// context.moveTo(400, 0);
-// context.lineTo(400, 600);
-// context.moveTo(500, 0);
-// context.lineTo(500, 600);
-// context.moveTo(600, 0);
-// context.lineTo(600, 600);
-// context.moveTo(700, 0);
-// context.lineTo(700, 600);
-// context.moveTo(800, 0);
-// context.lineTo(800, 600);
-// context.moveTo(900, 0);
-// context.lineTo(900, 600);
-// context.fill();
-// context.closePath();
-
-
-
-
-// context.stroke();
-// // context.fill()
-// context.closePath();
+}, 200);
